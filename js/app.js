dc1b8584988142b84a4df7493f343fb47076d885
= <=>
2021-12-31 14:11:37 +0530

    add no-cors

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ const load=(coun)=>{
     let country_population=document.getElementById("max_price");
     let country_area=document.getElementById("modal_price");
     
-    fetch('https://commodityapi.blob.core.windows.net/commodityapi/db.json', {mode: 'cors'}).then((data)=>{
+    fetch('https://commodityapi.blob.core.windows.net/commodityapi/db.json', {mode: 'no-cors', method: "get", headers: {"Content-Type": "application/json"}}).then((data)=>{
         return data.json()
     }).then((act)=>{
        
